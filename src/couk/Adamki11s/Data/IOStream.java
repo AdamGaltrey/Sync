a948d0c327c029de7deb1fb8deef841494e3fd03
Adamki11s <adam@adamki11s.co.uk>
2011-12-22 10:36:02 +0000

    Added Basic IO

---
+++ src/couk/Adamki11s/Data/IOStream.java
@@ -0,0 +1,74 @@
+package couk.Adamki11s.Data;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import couk.Adamki11s.Sync.Sync;
+
+public class IOStream {
+
+	private HashMap<String, Object> data = new HashMap<String, Object>();
+
+	private File f;
+
+	public IOStream(File f) {
+		this.f = f;
+	}
+
+	
+	/**
+	 * Writes all data to the file.
+	 */
+	public void write() {
+		try {
+			FileWriter fstream = new FileWriter(this.f);
+			BufferedWriter fbw = new BufferedWriter(fstream);
+			fbw.write("Generated Using Sync, Version " + Sync.version + ". Author : Adamki11s");
+			for (Entry<String, Object> entry : this.data.entrySet()) {
+				fbw.newLine();
+				fbw.write(entry.getKey().trim() + ":" + entry.getValue().toString().trim());
+			}
+			fbw.flush();
+			fbw.close();
+		} catch (IOException iox) {
+			iox.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reads all data from the file.
+	 */
+	public void read() {
+		try {
+			FileInputStream in = new FileInputStream(this.f);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				String[] property = strLine.split(":");
+				this.data.put(property[0], property[1]);
+			}
+			in.close();
+		} catch (IOException iox) {
+			iox.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Erases all data from the HashMap storage.
+	 */
+	public void erase(){
+		this.data.clear();
+	}
+
+}
--- src/couk/Adamki11s/Managers/IOManager.java
+++ src/couk/Adamki11s/Managers/IOManager.java
@@ -1,5 +1,9 @@
 package couk.Adamki11s.Managers;
 
+import java.io.File;
+
+import couk.Adamki11s.Data.IOStream;
+
 public class IOManager {
 	
 	SyncManager sm;
@@ -7,5 +11,15 @@ public class IOManager {
 	public IOManager(SyncManager sm) {
 		this.sm = sm;
 	}
+	
+	/**
+	 * Creates a new IOStream Instance
+	 * @param f The File which will be read and written to.
+	 * @return The IOStream
+	 */
+	public IOStream getIOStream(File f){
+		return new IOStream(f);
+	}
+	
 	
 }
