aa35d13204c77ad719bb0ed4a056aa293995fe1c
Adamki11s <adam@adamki11s.co.uk>
2011-12-22 01:08:43 +0000

    Added Required Methods

--- src/couk/Adamki11s/Sync/Sync.java
+++ src/couk/Adamki11s/Sync/Sync.java
@@ -1,7 +1,23 @@
 package couk.Adamki11s.Sync;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Sync extends JavaPlugin {
+	
+	private Logger log = Logger.getLogger("Sync");
+	private String version;
+
+	@Override
+	public void onDisable() {
+		log.info("[Sync] Sync Version " + version + " un-loaded successfully.");
+	}
+
+	@Override
+	public void onEnable() {
+		this.version = this.getDescription().getVersion();
+		log.info("[Sync] Sync Version " + version + " loaded successfully.");
+	}
 
 }
