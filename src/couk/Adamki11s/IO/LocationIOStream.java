cc186e8e2befa7754a55237e469f1e2bad25fcc7
Adamki11s <adam@adamki11s.co.uk>
2011-12-22 10:44:33 +0000

    Added Raw Implementation for saving Locations

---
+++ src/couk/Adamki11s/IO/LocationIOStream.java
@@ -0,0 +1,24 @@
+package couk.Adamki11s.IO;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class LocationIOStream extends IOStream {
+
+	public LocationIOStream(File f) {
+		super(f);
+	}
+	
+	public void write(){
+		super.write();
+	}
+	
+	public void read(){
+		super.read();
+	}
+	
+	public HashMap<String, Object> getData(){
+		return super.getData();
+	}
+
+}
