5064ab3b2f0e13c3781ec8a3e0d8154a60a01c98
Adamki11s <adam@adamki11s.co.uk>
2011-12-23 00:23:28 +0000

    Added Default Folder and Configuration Generation Methods

--- src/couk/Adamki11s/Managers/IOManager.java
+++ src/couk/Adamki11s/Managers/IOManager.java
@@ -1,6 +1,7 @@
 package couk.Adamki11s.Managers;
 
 import java.io.File;
+import java.io.IOException;
 
 import couk.Adamki11s.IO.IOStream;
 import couk.Adamki11s.IO.LocationIOStream;
@@ -24,6 +25,24 @@ public class IOManager {
 	
 	public LocationIOStream getLocationIOStream(File f){
 		return new LocationIOStream(f);
+	}
+	
+	public void createPluginFolder(){
+		File f = new File("plugins" + File.separator + sm.getPlugin().getDescription().getName());
+		if(!f.exists()){
+			f.mkdir();
+		}
+	}
+	
+	public void createPluginConfiguration(){
+		File f = new File("plugins" + File.separator + sm.getPlugin().getDescription().getName() + File.separator + "Configuration.yml");
+		if(!f.exists()){
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	
