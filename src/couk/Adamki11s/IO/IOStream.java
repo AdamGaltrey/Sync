5a2150ad3eb8569975732799d065d871d8d8becf
Adamki11s <adam@adamki11s.co.uk>
2012-01-09 01:04:06 +0000

    Added Base IO Read/Write Methods

---
+++ src/couk/Adamki11s/IO/IOStream.java
@@ -0,0 +1,16 @@
+package couk.Adamki11s.IO;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class IOStream {
+	
+	public void write(File f, HashMap<String, Object> data){
+		
+	}
+	
+	public HashMap<String, Object> read(File f){
+		return null;
+	}
+
+}
--- src/couk/Adamki11s/IO/SyncIO.java
+++ src/couk/Adamki11s/IO/SyncIO.java
@@ -2,10 +2,13 @@ package couk.Adamki11s.IO;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
-public class SyncIO {
+public class SyncIO extends IOStream {
 	
 	private File f;
+	private HashMap<String, Object> writeableData = new HashMap<String, Object>();
+	private HashMap<String, Object> readableData = new HashMap<String, Object>();
 	
 	public SyncIO(File f){
 		this.f = f;
@@ -25,6 +28,14 @@ public class SyncIO {
 	
 	public void delete(){
 		f.delete();
+	}
+	
+	public void write(){
+		super.write(this.f, this.writeableData);
+	}
+	
+	public void read(){
+		this.readableData = super.read(this.f);
 	}
 
 }
