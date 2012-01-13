53a146da7345ce1f0db63d46cfcbb7fb90793196
Adamki11s <adam@adamki11s.co.uk>
2012-01-13 00:02:05 +0000

    Added Cryptography Classes

--- src/couk/Adamki11s/Cryptography/SyncCryptography.java
+++ src/couk/Adamki11s/Cryptography/SyncCryptography.java
@@ -1,5 +1,18 @@
 package couk.Adamki11s.Cryptography;
 
 public class SyncCryptography {
+	
+	private HashEncoder hashEncoder = new HashEncoder();
+	private RSAEncoder rsaEncoder = new RSAEncoder();
+	
+	public HashEncoder getHashEncoder(){
+		return this.hashEncoder;
+	}
+	
+	public RSAEncoder getRSAEncoder(){
+		return this.rsaEncoder;
+	}
+	
+	
 
 }
