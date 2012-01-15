ef6f8407db97ea2a10bfec56c2a11febda1d1326
Adamki11s <adam@adamki11s.co.uk>
2012-01-15 23:05:15 +0000

    Removed RSA Encoder

--- src/couk/Adamki11s/Cryptography/SyncCryptography.java
+++ src/couk/Adamki11s/Cryptography/SyncCryptography.java
@@ -3,16 +3,10 @@ package couk.Adamki11s.Cryptography;
 public class SyncCryptography {
 	
 	private HashEncoder hashEncoder = new HashEncoder();
-	private RSAEncoder rsaEncoder = new RSAEncoder();
-	
-	public HashEncoder getHashEncoder(){
+
+	HashEncoder getHashEncoder(){
 		return this.hashEncoder;
 	}
-	
-	public RSAEncoder getRSAEncoder(){
-		return this.rsaEncoder;
-	}
-	
 	
 
 }
