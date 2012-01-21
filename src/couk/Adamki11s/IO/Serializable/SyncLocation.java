457215a6cd2698a920a48c06b425554f6356b120
Adamki11s <adam@adamki11s.co.uk>
2012-01-21 18:02:15 +0000

    Moved Serializable Package

---
+++ src/couk/Adamki11s/IO/Serializable/SyncLocation.java
@@ -0,0 +1,50 @@
+package couk.Adamki11s.IO.Serializable;
+
+import java.io.Serializable;
+
+import org.bukkit.Location;
+
+public class SyncLocation implements Serializable {
+
+	private static final long serialVersionUID = -6458678781007361236L;
+	
+	private String worldName;
+	private double x, y, z;
+	private float yaw, pitch;
+	
+	public SyncLocation(Location bukkitLocation){
+		this.worldName = bukkitLocation.getWorld().getName();
+		this.x = bukkitLocation.getX();
+		this.y = bukkitLocation.getY();
+		this.z = bukkitLocation.getZ();
+		this.yaw = bukkitLocation.getYaw();
+		this.pitch = bukkitLocation.getPitch();
+	}
+
+	public String getWorldName() {
+		return worldName;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+	
+	
+
+}
--- src/couk/Adamki11s/Serializable/SyncLocation.java
+++
@@ -1,9 +0,0 @@
-package couk.Adamki11s.Serializable;
-
-import java.io.Serializable;
-
-public class SyncLocation implements Serializable {
-
-	private static final long serialVersionUID = -6458678781007361236L;
-
-}
